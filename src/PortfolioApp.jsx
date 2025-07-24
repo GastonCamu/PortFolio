cca36c5c48afabe5f55d80009c135c32bb53e8ee
Gastón Camú <camugaston2002@gmail.com>
2025-07-24 12:18:32 -0300

    se cambio la version de react y react-dom a la 18 y se instalo prop-types

--- src/PortfolioApp.jsx
+++ src/PortfolioApp.jsx
@@ -1,9 +1,13 @@
+import PropTypes from 'prop-types';
+
 export function PortfolioApp({ title }) {
   return (
     <>
       <h1>{title}</h1>
     </>
   );
+};
 
-
-}
+PortfolioApp.propTypes = {
+  title: PropTypes.string.isRequired
+};
