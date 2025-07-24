9d4d0323cea24176f414a5b882cf50ddcbca67d9
Gastón Camú <camugaston2002@gmail.com>
2025-07-24 11:56:02 -0300

    creacion de archivo main.jsx, PortfolioApp.jsx, styles.css

---
+++ src/PortfolioApp.jsx
@@ -0,0 +1,9 @@
+export function PortfolioApp({ title }) {
+  return (
+    <>
+      <h1>{title}</h1>
+    </>
+  );
+
+
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, { StrictMode } from 'react';
+import ReactDOM, { createRoot } from 'react-dom/client';
+
+import { PortfolioApp } from './PortfolioApp';
+import './styles.css';
+
+const title = 'Hola, me llamo Gastón Camú y soy desarrollador Front End';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <PortfolioApp title={title} />
+  </StrictMode>
+);
